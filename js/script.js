76dc5170a2df99f76f867c5cee29b36631274005
Zahra <zhrbbzd@gmail.com>
2023-01-23 16:58:23 +0330

    Add product mini project TAFTACO

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Project loaded add product page dont show when onclock add product button that be show
+let addNewProduct = document.querySelector("#addNewProduct")
+addNewProduct.style.display = "none"
+function addNewProductPage(){
+    addNewProduct.style.display = "block";
+}
+
+// When click on checking fixed transportation show transportation cost
+var transprtationCost = document.querySelector(".transportaionCost")
+let fixedCostInputox = document.querySelector("#fixedCostInput");
+fixedCostInput.checked = false;
+fixedCostInput.onchange = function() {
+    transprtationCost.style.display = this.checked ? 'block' : 'none';
+};
+fixedCostInput.onchange();
+
+// Navbar toggle
+$("#burgerMenuIcon").click(function(){
+    $("#responsiveSidebar").toggle()
+})
+
+let resAddProductLink = document.querySelector("#resAddProductLink")
+let responsiveMain = document.querySelector("#responsiveMain")
+resAddProductLink.addEventListener("click", e => {
+    responsiveMain.style.display = "none" ? 'block' : 'none'
+})
